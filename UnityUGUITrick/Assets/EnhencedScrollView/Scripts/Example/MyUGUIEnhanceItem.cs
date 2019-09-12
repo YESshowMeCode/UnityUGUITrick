69f76798efb40c6aa4838914a1e184900a5b72d3
Chengaoshuang <79480311@qq.com>
2019-09-12 22:57:35 +0800

    123

---
+++ UnityUGUITrick/Assets/EnhencedScrollView/Scripts/Example/MyUGUIEnhanceItem.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MyUGUIEnhanceItem : EnhanceItem
+{
+    private Button uButton;
+    private Image rawImage;
+
+    protected override void OnStart()
+    {
+        rawImage = GetComponent<Image>();
+        uButton = GetComponent<Button>();
+        uButton.onClick.AddListener(OnClickUGUIButton);
+    }
+
+    private void OnClickUGUIButton()
+    {
+        OnClickEnhanceItem();
+    }
+
+    // Set the item "depth" 2d or 3d
+    protected override void SetItemDepth(float depthCurveValue, int depthFactor, float itemCount)
+    {
+        int newDepth = (int)(depthCurveValue * itemCount);
+        this.transform.SetSiblingIndex(newDepth);
+    }
+
+    public override void SetSelectState(bool isCenter)
+    {
+        if (rawImage == null)
+            rawImage = GetComponent<Image>();
+        rawImage.color = isCenter ? Color.white : Color.gray;
+    }
+}
